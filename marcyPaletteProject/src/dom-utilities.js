f289dee7bb35b6e6e9e1f640bed231b50d21c7b7
tailsmster <lotailsmonsieur62c900@gmail.com>
2023-12-11 15:41:03 -0500

    popeyes chicken isnt good for me, but wow does it taste good

--- marcyPaletteProject/src/dom-utilities.js
+++ marcyPaletteProject/src/dom-utilities.js
@@ -1,8 +1,39 @@
 import { getPalettes } from './local-storage'
-import paletteSetup from './palettes.json'
+
+const colorDisplay = (colorCode) => {
+    const colorBox = document.createElement('body');
+    const colorHex = document.createElement('p');
+    colorHex.innerHTML = '<span style="color: black;">peepee</span> poopoo';
+    colorHex.classList.add('color')
+    colorHex.style.background=colorCode
+
+    const colorCopy = document.createElement('p');
+    colorCopy.innerText = `Copy \n${colorCode}`;
+    colorCopy.classList.add('color-copy');
+    colorCopy.dataset.color = colorCode;
+
+    colorContainer.classList.add('color-container');
+    colorContainer.append(colorText, colorCopy);
+    return colorContainer;
+}
 
 const renderSinglePalette = (x, data) => {
-    
+    const { title, color1, color2, color3, temperature, uuid } = data
+    //palette title
+    const li = document.createElement('li');
+    li.dataset.uuid = uuid;
+    const h2 = document.createElement('h2');
+    h2.innerText = title;
+
+    //colors
+    li.append(h3);
+    colors.forEach((color) => li.append(createColorDiv(color)))
+
+    //temps
+    const temperatureText = document.createElement('h5');
+    temperatureText.innerText = temperature;
+    temperatureText.classList.add('temperature')
+    temperatureText.classList.add(temperature)
 }
 
 const renderAllPalettes = () => {
--- marcyPaletteProject/src/main.js
+++ marcyPaletteProject/src/main.js
@@ -2,12 +2,32 @@ import './style.css'
 import { v4 as uuidv4 } from 'uuid';
 import { addPalette, removePalette} from './local-storage';
 import { renderAllPalettes } from './dom-utilities';
+import palettes from './palettes.json'
 // console.log(palettes); 
 
 
 
 //helper functions
 
+const handleDeletePalette = (e) => {
+  if (!e.target.matches('.delete-palette')) return;
+  const uuid = e.target.parentElement.dataset.uuid;
+  deletePalette(uuid);
+  renderPalettes();
+}
+
+const handleColorCopy = async (e) => {
+  if (!navigator.clipboard) return;
+  if (!e.target.matches('.color-copy')) return;
+  try {
+    const { dataset, textContent } = e.target;
+    await navigator.clipboard.writeText(dataset.color);
+    e.target.textContent = 'Copied to clipboard!';
+    setTimeout(() => { e.target.textContent = textContent }, 1000)
+  } catch (err) {
+    console.error('Failed to copy!', err);
+  }
+}
 
 const handleFormSubmit = (e) => {
   e.preventDefault(); 
@@ -20,19 +40,24 @@ const handleFormSubmit = (e) => {
     uuid:uuidv4(),
     title: formObj.title,
     colors: [formObj.color1, formObj.color2, formObj.color3],
-    temperature: formObj.temperature
+    temperature: formObj.temperature,
   };
 
-  addPalette(newPalette)
+  addPalette(newPalette);
   renderAllPalettes();
 
   form.reset();
-}
+};
 
 //runner function
 const main = () => {
-    const form = document.querySelector("#new-palette-form")
-    form.addEventListener('submit', handleSubmit)
+  renderPalettes();
+  
+  const form = document.querySelector("#new-palette-form");
+  form.addEventListener('submit', handleSubmit);
+
+  const ul = document.querySelector('#palettes-list');
+  ul.addEventListener('click', handleUlClick);
 }
 
 main();
